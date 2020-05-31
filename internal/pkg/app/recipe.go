41554cca4341cd5ddf0162f6599bef96bd2bb4d2
Ian Feather <info@ianfeather.co.uk>
2020-05-31 13:27:32 +0100

    return json

--- internal/pkg/app/recipe.go
+++ internal/pkg/app/recipe.go
@@ -1,9 +1,21 @@
 package app
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
+// Recipe contains recipe fields
+type Recipe struct {
+	Name        string
+	Ingredients int
+}
+
 func (a *App) recipeHandler(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Recipe page"))
+	recipe := Recipe{"French Toast", 4}
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(recipe)
+	if err != nil {
+		w.Write([]byte("Error encoding json"))
+	}
 }
