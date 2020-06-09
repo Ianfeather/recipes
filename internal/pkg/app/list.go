3b87ea907227603534a3902e3bac88e2d84a5c25
Ian Feather <info@ianfeather.co.uk>
2020-06-09 23:55:20 +0100

    Combine same unit ingredients

--- internal/pkg/app/list.go
+++ internal/pkg/app/list.go
@@ -9,16 +9,46 @@ import (
 	"strings"
 )
 
-// RecipeList is a list of recipes
-type RecipeList struct {
-	Recipes []common.Recipe `json:"recipes"`
+// ResponseObject is the json response
+type ResponseObject struct {
+	Recipes []common.Recipe        `json:"recipes"`
+	List    map[string]*Ingredient `json:"list"`
+}
+
+// Ingredient is a subset of shopping List
+type Ingredient struct {
+	Unit     string  `json:"unit"`
+	Quantity float64 `json:"quantity"`
+}
+
+func combineIngredients(r []common.Recipe) map[string]*Ingredient {
+	ingredientList := make(map[string]*Ingredient)
+
+	for _, recipe := range r {
+		for _, ingredient := range recipe.Ingredients {
+			existingIngredient, exists := ingredientList[ingredient.Name]
+
+			if q, err := strconv.ParseFloat(ingredient.Quantity, 64); err == nil {
+				if exists {
+					existingIngredient.Quantity = existingIngredient.Quantity + q
+				} else {
+					newIngredient := Ingredient{
+						Unit:     ingredient.Unit,
+						Quantity: q,
+					}
+					ingredientList[ingredient.Name] = &newIngredient
+				}
+			}
+		}
+	}
+	return ingredientList
 }
 
 func (a *App) getListHandler(w http.ResponseWriter, req *http.Request) {
 	qs := req.URL.Query()
 	recipeIDs := strings.Split(qs.Get("recipes"), ",")
 
-	recipeList := &RecipeList{}
+	response := &ResponseObject{}
 
 	for i := 0; i < len(recipeIDs); i++ {
 		id, err := strconv.Atoi(recipeIDs[i])
@@ -29,11 +59,13 @@ func (a *App) getListHandler(w http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			// do something
 		}
-		recipeList.Recipes = append(recipeList.Recipes, *recipe)
+		response.Recipes = append(response.Recipes, *recipe)
 	}
 
+	response.List = combineIngredients(response.Recipes)
+
 	encoder := json.NewEncoder(w)
-	err := encoder.Encode(recipeList)
+	err := encoder.Encode(response)
 	if err != nil {
 		http.Error(w, "Error encoding json", http.StatusInternalServerError)
 		return
