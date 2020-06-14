489ea9e2417d7eb32c83643f6085c21f529fe96a
Ian Feather <info@ianfeather.co.uk>
2020-06-14 16:56:49 +0100

    Working insert

--- internal/pkg/service/recipe.go
+++ internal/pkg/service/recipe.go
@@ -72,15 +72,14 @@ func GetRecipeByID(id int, db *sql.DB) (r *common.Recipe, e error) {
 // AddRecipe inserts recipe, ingredients into the DB
 func AddRecipe(recipe common.Recipe, db *sql.DB) error {
 
-	_, err := db.Query(fmt.Sprintf("INSERT INTO recipe (name, slug, remote_url) VALUES ('%s', '%s', '%s')", recipe.Name, common.Slugify(recipe.Name), recipe.RemoteURL))
+	res, err := db.Exec(fmt.Sprintf("INSERT INTO recipe (name, slug, remote_url) VALUES ('%s', '%s', '%s')", recipe.Name, common.Slugify(recipe.Name), recipe.RemoteURL))
 
 	if err != nil {
 		fmt.Println("could not insert recipe")
-		fmt.Println(err.Error())
 		return err
 	}
 
-	fmt.Println(recipe.Ingredients)
+	recipeID, err := res.LastInsertId()
 
 	ingredientQuery := "INSERT INTO ingredient (name) values "
 	for idx, ingredient := range recipe.Ingredients {
@@ -90,13 +89,36 @@ func AddRecipe(recipe common.Recipe, db *sql.DB) error {
 		}
 	}
 	ingredientQuery += " ON DUPLICATE KEY UPDATE id=id;"
-	fmt.Println(ingredientQuery)
-	_, err = db.Query(ingredientQuery)
 
+	_, err = db.Exec(ingredientQuery)
 	if err != nil {
 		fmt.Println("could not insert ingredients")
-		fmt.Println(err.Error())
 		return err
 	}
+
+	// insert the connection
+
+	partsQuery := "INSERT INTO part (recipe_id, ingredient_id, unit_id, quantity) VALUES "
+	for idx, ingredient := range recipe.Ingredients {
+		partsQuery += fmt.Sprintf("(%d, ", recipeID)
+		partsQuery += fmt.Sprintf("(SELECT id FROM ingredient WHERE name = '%s'),", ingredient.Name)
+		partsQuery += fmt.Sprintf("(SELECT id FROM unit WHERE name = '%s'),", ingredient.Unit)
+		partsQuery += fmt.Sprintf("%s) ", ingredient.Quantity)
+		if idx != len(recipe.Ingredients)-1 {
+			partsQuery += ","
+		} else {
+			partsQuery += ";"
+		}
+	}
+	fmt.Println("****partsQuery")
+	fmt.Println(partsQuery)
+	fmt.Println("****partsQuery")
+
+	_, err = db.Exec(partsQuery)
+	if err != nil {
+		fmt.Println("could not insert part")
+		return err
+	}
+
 	return nil
 }
