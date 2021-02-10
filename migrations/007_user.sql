938f7f1ad62a094bc5d92848c29603ebb75d6a74
Ian Feather <info@ianfeather.co.uk>
2021-02-10 23:19:23 +0000

    migration

--- migrations/007_user.sql
+++ migrations/007_user.sql
@@ -2,3 +2,4 @@ DROP TABLE recipe_user;
 DROP TABLE user;
 ALTER TABLE recipe ADD user_id varchar(255) NOT NULL;
 ALTER TABLE list MODIFY user_id varchar(255) NOT NULL;
+update recipe set user_id="google-oauth2|100337785987015262344" where user_id = "";
